5ee559275073510234d21d08bfb39006f07f4b01
Russell Roberts <russrob23@gmail.com>
2017-01-17 12:27:55 -0800

    first commit and added github api GET request and getuser

---
+++ app/services/github.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService{
+	private username:string;
+	private client_id: '9e0991b034d55f6cc6dd';
+	private client_secret:'ecfbf044be9e4e9566f9a469aea82a864fe84b11';
+
+	constructor(private _http: Http){
+		console.log('Github Service is Ready...');
+		this.username = 'russrob';
+	}
+
+	getUser(){
+		return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+		.map(res => res.json());
+	}
+}
+
