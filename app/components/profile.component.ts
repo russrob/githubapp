c068d83087c2d261ab6d6ea08faae566d619c9d7
Russell Roberts <russrob23@gmail.com>
2017-01-17 13:38:06 -0800

    displasy users data and repos and create a panel and added a new api request

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -8,10 +8,15 @@ import {GithubService} from '../services/github.service';
 })
 export class ProfileComponent  { 
 	user:any[];
+	repos:any[];
 	constructor(private _githubService: GithubService){
 
 		this._githubService.getUser().subscribe(user => {
 			this.user = user; 
+		});
+
+		this._githubService.getRepos().subscribe(repos => {
+			this.repos = repos; 
 		});
 	}
 
